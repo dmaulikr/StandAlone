d33357e378cdf3957d8bf3224a000b36e55567cf
kumar5979 <kavekumar@gmail.com>
2014-02-21 21:17:41 -0600

    Initial Commit

---
+++ StandAloneApp/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  StandAloneApp
+//
+//  Created by kumar veerappan on 2/21/14.
+//  Copyright (c) 2014 kumar veerappan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
